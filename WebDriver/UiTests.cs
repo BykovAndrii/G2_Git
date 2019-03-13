ae34c6f8f0ad51134d58f2d273dcd7c33404b381
qalight <terraco_kiev@ukr.net>
2019-03-13 20:30:32 +0200

    new

--- WebDriver/UiTests.cs
+++ WebDriver/UiTests.cs
@@ -1,9 +1,15 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace WebDriver
 {
     public class UiTests
     {
-
+        [Test]
+        public void TestWebDriverMethods()
+        {
+            IWebDriver driver = new ChromeDriver();
+        }
     }
 }
