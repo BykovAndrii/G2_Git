ae43a7d1742a54d936c0107ebe500ae279d994e9
qalight <terraco_kiev@ukr.net>
2019-04-17 20:54:25 +0300

    add db project

---
+++ HW/Dependency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW
+{
+    interface ILogger
+    {
+        void LoggerLog();
+    }
+
+    class Logger : ILogger
+    {
+        public void LoggerLog()
+        {
+            Console.WriteLine("From logger"); 
+        }
+    }
+
+    class NewLogger : ILogger
+    {
+        public void LoggerLog()
+        {
+            Console.WriteLine("From new logger");
+        }
+    }
+
+    class Dependency
+    {
+        private ILogger internalLogger;
+
+        public Dependency(ILogger logger)
+        {
+            internalLogger = logger;
+        }
+
+        public void Log()
+        {
+            internalLogger.LoggerLog();
+        }
+    }
+}
---
+++ JenkinsProject/UT.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace JenkinsProject
+{
+    public class UT
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("For testing run from Jenkins 11");
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            //Assert.True(false);
+            Console.WriteLine("For testing run from Jenkins 22");
+        }
+    }
+}
